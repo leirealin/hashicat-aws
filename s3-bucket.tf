b215e38373f9fd2c10882e9d9d7f29232fd420f1
leirealin <lei_lin@fanniemae.com>
2023-01-30 20:08:57 +0000

    Updated s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/fanniemae-aws2/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix="LEILIN"
-
-  versioning = {
-    enabled = true
-  }
 
 }
